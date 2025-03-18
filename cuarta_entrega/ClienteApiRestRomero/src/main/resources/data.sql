4f28df4c7d2f75528ad34d9fd77099fe7643158b
Jeshua-Romero-Guadarrama <jeshua.romero@outlook.com>
2025-03-18 06:46:50 -0600

    Jeshua

--- cuarta_entrega/ClienteApiRestRomero/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO cliente (id, nombre, apellido, fecha_nacimiento) 
-VALUES (1, 'Juan', 'Gomez', '1990-05-23');
-
-INSERT INTO cliente (id, nombre, apellido, fecha_nacimiento) 
-VALUES (2, 'Maria', 'Perez', '1985-10-11');
-
-INSERT INTO cliente (id, nombre, apellido, fecha_nacimiento) 
-VALUES (3, 'Carlos', 'Lopez', '1992-07-15');
